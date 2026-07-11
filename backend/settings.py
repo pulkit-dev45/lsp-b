aafb884333e34b31816ea64fa3855c99a71b23d8
Manav <manavk4214@gmail.com>
2026-07-11 20:49:34 +0530

    cors header policy change

--- backend/settings.py
+++ backend/settings.py
@@ -37,12 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'lsp_auth',
     'courses',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +128,6 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
 }
+CORS_ALLOWED_ORIGINS=["http://localhost:5173"]
+
+CORS_ALLOW_CREDENTIALS = True
